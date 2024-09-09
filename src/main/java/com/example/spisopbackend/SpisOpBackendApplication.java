4012c97985708d325cd44fce8ba6a490cf6fe4d8
albe194e <albe194e@stud.kea.dk>
2024-09-09 13:00:58 +0200

    First

---
+++ src/main/java/com/example/spisopbackend/SpisOpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.spisopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpisOpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpisOpBackendApplication.class, args);
+	}
+
+}
