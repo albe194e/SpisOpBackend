583c776f357e4fbf4de23bda0ab8760ca6534ea1
albe194e <albe194e@stud.kea.dk>
2024-09-18 09:48:05 +0200

    Controllers

---
+++ src/main/java/com/example/spisopbackend/controller/AddressController.java
@@ -0,0 +1,8 @@
+package com.example.spisopbackend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AddressController {
+
+}
---
+++ src/main/java/com/example/spisopbackend/controller/CommunityController.java
@@ -0,0 +1,10 @@
+package com.example.spisopbackend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CommunityController {
+
+
+
+}
