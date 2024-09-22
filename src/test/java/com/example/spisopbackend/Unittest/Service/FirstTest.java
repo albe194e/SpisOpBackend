6f5b1faaaa0ef814f9244f10d4113a8fb80b218f
albe194e <albe194e@stud.kea.dk>
2024-09-22 16:04:19 +0200

    add test test

---
+++ src/test/java/com/example/spisopbackend/Unittest/Service/FirstTest.java
@@ -0,0 +1,17 @@
+package com.example.spisopbackend.Unittest.Service;
+
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class FirstTest {
+
+
+    @Test
+    void test(){
+
+        assertEquals(1,1);
+    }
+}
